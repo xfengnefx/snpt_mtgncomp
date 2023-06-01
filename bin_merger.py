6467d7e6acc3a28ed92c790d645a82afe1c7a3e3
xfeng <xfengnefx@gmail.com>
2023-06-01 08:58:39 -0400

    updates at rev1

--- bin_merger.py
+++ bin_merger.py
@@ -1,8 +1,4 @@
-import sys, argparse, os, stat, statistics
-from fileutil import opener
-from readfq import readfq
-from time import time
-
+import argparse
 example_text = '''example:
    python {0} <(gfa2l.py asm.gfa) asm.rescue.fa /path/metabat2/
    python {0} contiglens.tsv asm.rescue.fa.gz /path/metabat2
@@ -11,39 +7,36 @@ example_text = '''example:
 parser = argparse.ArgumentParser(description="Merge rescued circles and genome binner bins, write to stdout or specified file.",
                                 epilog=example_text,
                                 formatter_class=argparse.RawDescriptionHelpFormatter)
-parser.add_argument('-l', type=str, default='', help='a file conting a list of contigs, will report any of them if not used by rescueing *and* binner.')
-parser.add_argument('-x', type=str, default='fa', help='file suffix of binner bins. Ignored if files instead of a directory are specified. ')
+parser.add_argument('-x', type=str, default='fa', help='file suffix of binner bins, e.g. use fa for *.fa files. Ignored if files instead of a directory are specified.')
 parser.add_argument('-o', type=str, default='', help='write output to this file instead of stdout')
-parser.add_argument('fn_contigl_tsv', type=str, help='a tab-delimited file. col1=contig names, col2=contig lengths')
-parser.add_argument('fn_rescue_fasta', type=str, help='fasta file of rescued circles')
+parser.add_argument('fn_contigl_tsv', type=str, help='a tab-delimited file. col1=contig names, col2=contig lengths.')
+parser.add_argument('fn_rescue_fasta', type=str, help='fasta file of rescued circles.')
 parser.add_argument('binner', nargs='+', type=str, help='folder contains binner bins or fasta/q files, can be mixed. Will assume plain text if file is a pipe.')
 args = parser.parse_args()
+
+import sys, argparse, os, stat, statistics
+from fileutil import opener
+from readfq import readfq
+from time import time
+
+
 
 accept_list = {}  # [type, size, n_contigs, contignames]
 blacklist = set()
 bin2tig = {}
-maptignames = {}
 reject = []
-ll = {}
+ll = {}  # tig2len
 long_linear_contigs = set()
-threshold = 10
 
 # collect contig lengths
-# also record truncated names - TODO update hifiasm-meta
 T = time()
 with open(args.fn_contigl_tsv) as file:
     for line in file:
         qn, l = line.strip().split('\t')
         l = int(l)
         qnshort = qn.split('.')[1]
-        maptignames[qnshort] = qn
         ll[qn] = l
-        
-# anything to preserve?
-if args.l!='':
-    file = opener(args.l)
-    for line in file:
-        long_linear_contigs.add(line.strip())
+
 sys.stderr.write('[M] prepare done. %.1fs\n'%(time()-T))
 
 # record rescued circles
@@ -52,14 +45,12 @@ with open(args.fn_rescue_fasta) as file:
     for name, seq, qual in readfq(file, get_full_name=True):
         name = name.strip().split(' ')
         pfname = name[0]
-        tigs = [maptignames[_[:-1]+'l'] for _ in name[1:]]
+        tigs = [_[:-1] for _ in name[1:]]
         bin2tig[pfname] = tigs
         tigs_ll = [ll[tig] for tig in tigs]
-        if (len(tigs)>=threshold and statistics.median(tigs_ll)<500000) or\
-           len([_ for _ in tigs_ll if _>100000])>2:
-            accept_list[pfname] = ['rc', len(seq), 1, pfname]
-            for tig in tigs:
-                blacklist.add(tig)
+        accept_list[pfname] = ['rc', len(seq), 1, pfname]
+        for tig in tigs:
+            blacklist.add(tig)
 sys.stderr.write(f'[M] accept rescue circles: {len(accept_list)} (%.1fs)\n'%(time()-T))
 
 # record binner bins
@@ -88,37 +79,41 @@ def yield_binner_streams(fns, suffix='fa'):
     sys.stderr.write(f'[M] processed {n} files ({nskip} ignored) from binner\n')
 for b, stream in yield_binner_streams(args.binner):
     tigs = []
-    for line in stream:
-        for qname, seq, qual in readfq(stream):
-            tigs.append(qname)
-        if len(tigs)>0:
-            bin2tig[b] = tigs
-            
+    for qname, seq, qual in readfq(stream):
+        tigs.append(qname)
+    if len(tigs)>0:
+        bin2tig[b] = tigs
+    else:
+        continue
+
     binsize = sum([ll[tig] for tig in tigs])
     if binsize<500000 or binsize>10000000:
         reject.append(b)
         continue
 
     used = [_ for _ in tigs if _ in blacklist]
-    if len(used)/len(tigs)>0.1 and len(used)>5:
+    used_bp = sum([ll[tig] for tig in used])
+    if used_bp>1e6 or len(used)>10:
         reject.append(b)
     else:
+        for tig in tigs:
+            blacklist.add(tig)
         accept_list[b] = ['binner', binsize, len(tigs), ','.join(tigs)]
 sys.stderr.write('[M] accepted total: {0}; rejected from binner: {1}. ({2})\n'.format(
-                len(accept_list), 
-                len(reject), 
+                len(accept_list),
+                len(reject),
                 '%.1fs'%(time()-T)))
- 
+
 # output
 if args.o!='':
     file_out = open(args.o, 'w')
 else:
-    file_out = sys.stderr        
+    file_out = sys.stderr
 file_out.write('\t'.join(['#binname', 'type', 'size', 'n_contigs', 'contigs'])+'\n')
 for b in accept_list:
     tmp = [b]+[str(_) for _ in accept_list[b]]
     file_out.write('\t'.join(tmp)+'\n')
 if args.o!='':
     file_out.close()
-    
-       
+
+
--- plotkmerspectrum.py
+++ plotkmerspectrum.py
@@ -3,109 +3,29 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-parser = argparse.ArgumentParser(description='Plot right-accumulated raio plot of a k-mer profile.')
+parser = argparse.ArgumentParser(description='Plot right-accumulated raio plot of a k-mer profile. Demo takes one file; see other functions for comparison between files (hatched regions; up to three files).')
 parser.add_argument('f_in', type=str, help='The k-mer profile.')
 parser.add_argument('f_out', type=str, help='Output file name.')
 args = parser.parse_args()
 
 
-def kat_typetall(f, ax,  fzero='', separator=',', ignore_lastline=True):
-    # a very tall plot showing absolute counts 
-    with open(f) as file:
-        d = []
-        for line in file:
-            if line[0]=='#': continue
-            line = line.strip().split(separator)
-            d.append([int(_) for _ in line if _!=''])
-    d = np.array(d)
-    d = d.T
-    print(d.shape)
-
-    LIM = 15
-    bottom = np.zeros(len(d[0]))
-    L = len(bottom)
-    for i, _ in enumerate(d[:LIM]):
-        ax.bar(np.arange(L), d[i],bottom=bottom)
-        bottom += d[i]
-    #     plt.bar(np.arange(L), np.log10(d[i]+1),bottom=bottom)
-    #     bottom += np.log10(d[i]+1)
-
-    tmp = np.sum(d[LIM:], axis=0)
-    ax.bar(np.arange(L), tmp, bottom = bottom, label='+')
-    # plt.bar(np.arange(L), np.log10(tmp+1), bottom = bottom, label='+')
-
-    ##### zero dump
-    if fzero!='':
-        with open(fzero, 'rb') as file:
-            zerotot, zero_n_one_mismatch, zero_data, zero_data_one_mismatch = pickle.load(file)
-        d_zero = []
-        for i in range(d.shape[0]):
-            if i in zero_data: d_zero.append(zero_data[i])
-            else: d_zero.append(0)
-        d_zero = np.array(d_zero)
-        ax.bar(np.arange(L), d_zero, bottom =d[0]-d_zero, color='gray')
-    ###########
-
-    ax.set_ylim(bottom=0, top=1000000)
-
-
-def kat_type1(f, ax,
-              fzero='', separator=',', ignore_lastline=True):
-    # plain ratio plot
-    with open(f) as file:
-        d = []
-        for line in file:
-            if line[0]=='#': continue
-            line = line.strip().split(separator)
-            d.append([int(_) for _ in line if _!=''])
-    if ignore_lastline: d = np.array(d[:-1])
-    else: d = np.array(d)
-
-    d2 = []
-    for _ in d:
-        tmp = _/np.sum(_)
-        d2.append(tmp)
-    d2 = np.array(d2)
-    d2 = d2.T
-
-    LIM = 15
-    bottom = np.zeros(len(d2[0]))
-    L = len(bottom)
-    for i, _ in enumerate(d2[:LIM]):
-        ax.plot(d2[i]+bottom)
-        ax.fill_between(np.arange(L), d2[i]+bottom,bottom)
-        bottom += d2[i]
-
-    tmp = np.sum(d2[LIM:], axis=0)
-    ax.plot(tmp+bottom)
-    ax.fill_between(np.arange(L), tmp+bottom, bottom)
-
-    ##### zero dump
-    if fzero!='':
-        with open(fzero, 'rb') as file:
-            zerotot, zero_n_one_mismatch, zero_data, zero_data_one_mismatch = pickle.load(file)
-        d_zero = []
-        for i in range(d.shape[0]):
-            if i in zero_data: 
-                d_zero.append(zero_data[i]/sum(d[i]))
-            else: d_zero.append(0)
-        d_zero = np.array(d_zero)
-        ax.plot(d2[0], color='gray')
-        ax.fill_between(np.arange(L), d2[0], d2[0]-d_zero, color='gray')
-    ###########
-    ax.set_title(f+' | type1')
 
 def kat_type2(f, ax,
               write_prefix='', show=True,
               fzero='', separator=',', ignore_lastline=True,
-              title=''):
-    # right-accumulated ratio plot
+              title='',
+              no_1xabove=False  # do not plot the "colorful" bands
+             ):
+    # accumulated ratio plot
     with open(f) as file:
         d = []
         for line in file:
             if line[0]=='#': continue
             line = line.strip().split(separator)
-            d.append([int(_) for _ in line if _!=''])
+            tmp = [int(_) for _ in line if _!='']
+            tmp = [0 if _<0 else _ for _ in tmp ]  # monkey patch at first column - need to fix the overflow
+            d.append(tmp)
+
     d = np.array(d)
 
     d2 = []
@@ -129,27 +49,136 @@ def kat_type2(f, ax,
             alpha=1
         if i==0:
             ret = d2[i]+bottom
-        ax.plot(d2[i]+bottom, alpha=alpha)
-        ax.fill_between(np.arange(L), d2[i]+bottom,bottom, alpha=alpha)
+        if not no_1xabove or (no_1xabove and i<=1):
+            ax.plot(d2[i]+bottom, alpha=alpha)
+            ax.fill_between(np.arange(L), d2[i]+bottom,bottom, alpha=alpha)
         bottom += d2[i]
 
     tmp = np.sum(d2[LIM:], axis=0)
-    ax.plot(tmp+bottom, alpha=alpha, color='gray')
-    ax.fill_between(np.arange(L), tmp+bottom, bottom, 
-                    alpha=alpha, color='gray')
-    
-    
-    ##### absolute count right-accumulated curve
+    if not no_1xabove:
+        ax.plot(tmp+bottom, alpha=alpha, color='gray')
+        ax.fill_between(np.arange(L), tmp+bottom, bottom,
+                        alpha=alpha, color='gray')
+
+
+    # absolute count right accumulated
     a = np.sum(d, axis=1)
     a = a[::-1]
     aa = [a[0]]
     for i in range(1, len(a)):
         aa.append(aa[i-1]+a[i])
     aa = np.array(aa[::-1])
+    for i_whiteout in range(len(aa)):
+        if aa[i_whiteout]<1e6:break
+
+    ##### absolute count right-accumulated curve
     ax2 = ax.twinx()
     ax2.plot(aa, color='white', linewidth=1, linestyle='dashed')
     ax2.set_yscale('log')
-    
+
+
+#     ##### soft white out
+#     ax.fill_between(range(i_whiteout, len(aa)),
+#                     [0]*(len(aa)-i_whiteout),
+#                     [1]*(len(aa)-i_whiteout),
+#                     color='white',
+#                     alpha=0.8
+#                    )
+    ##### alternative: hard white out
+    ax.fill_between(range(i_whiteout, len(aa)),
+                    [0]*(len(aa)-i_whiteout),
+                    [1]*(len(aa)-i_whiteout),
+                    color='white',
+                    alpha=1, zorder=2
+                   )
+#     #### alternative, put a red vertical bar
+#     for i_whiteout in range(len(aa)):
+#         if aa[i_whiteout]<1e6:break
+#     ax.axvline(i_whiteout, color='red', alpha=0.5, linestyle='dashed')
+
+
+    if title=='':
+        ax.set_title(f+' | type2')
+    else:
+        ax.set_title(title)
+    return ret, aa
+
+
+def kat_type3(f_prev, f_new, ax,
+              write_prefix='', show=True,
+              fzero='', separator=',', ignore_lastline=True,
+              title='',
+             no_1xabove=False):
+    data = []
+    for f in [f_prev, f_new]:
+        # accumulated ratio plot
+        with open(f) as file:
+            d = []
+            for line in file:
+                if line[0]=='#': continue
+                line = line.strip().split(separator)
+                tmp = [int(_) for _ in line if _!='']
+                tmp = [0 if _<0 else _ for _ in tmp ]  # monkey patch - need to fix the overflow
+                d.append(tmp)
+
+        d = np.array(d)
+
+        d2 = []
+        for i in range(d.shape[0]-1):
+            tmp = d[i:, :].sum(axis=0)
+            tmp = tmp/(np.sum(tmp)+1)
+            d2.append(tmp)
+        d2.append(d[-1, :]/(np.sum(d[-1, :])+1))
+        d2 = np.array(d2)
+        d2 = d2.T
+        data.append(d2)
+
+
+    LIM = 15
+    # get the watermark from old run
+    watermark = data[0][0]
+    # plot new run
+    bottom = np.zeros(len(d2[0]))
+    L = len(bottom)
+    ret = ''
+    for i, _ in enumerate(d2[:LIM]):
+        if i>1:
+            alpha=0.3
+        else:
+            alpha=1
+        if i==0:
+            ret = d2[i]+bottom
+        if not no_1xabove or (no_1xabove and i<=1):
+            ax.plot(d2[i]+bottom, alpha=alpha)
+            ax.fill_between(np.arange(L), d2[i]+bottom,bottom, alpha=alpha,
+                           linewidth=0.2)
+        bottom += d2[i]
+
+    tmp = np.sum(d2[LIM:], axis=0)
+    if not no_1xabove:
+        ax.plot(tmp+bottom, alpha=alpha, color='gray')
+        ax.fill_between(np.arange(L), tmp+bottom, bottom, linewidth=0.2,
+                        alpha=alpha, color='gray')
+    # plot watermark
+    ax.fill_between(np.arange(L), data[1][0], data[0][0], hatch='|',
+                          linewidth=0.2,facecolor="#ffa154", edgecolor='black')
+#     ax_water.plot(watermark, color='white', linewidth=1, alpha=0.7)
+
+    # absolute count right accumulated
+    a = np.sum(d, axis=1)
+    a = a[::-1]
+    aa = [a[0]]
+    for i in range(1, len(a)):
+        aa.append(aa[i-1]+a[i])
+    aa = np.array(aa[::-1])
+    for i_whiteout in range(len(aa)):
+        if aa[i_whiteout]<1e6:break
+
+    ##### absolute count right-accumulated curve
+    ax2 = ax.twinx()
+    ax2.plot(aa, color='white', linewidth=1, linestyle='dashed')
+    ax2.set_yscale('log')
+
 
     ##### zero dump
     if fzero!='':
@@ -157,34 +186,135 @@ def kat_type2(f, ax,
             zerotot, zero_n_one_mismatch, zero_data, zero_data_one_mismatch = pickle.load(file)
         d_zero = []
         for i in range(d.shape[0]):
-            if i in zero_data: 
+            if i in zero_data:
                 d_zero.append(zero_data[i]/sum(d[i:, 0]))
             else: d_zero.append(0)
         d_zero = np.array(d_zero)
         ax.plot(d2[0], color='gray')
         ax.fill_between(np.arange(L), d2[0], d2[0]-d_zero, color='gray')
     ###########
-    
-#     ##### white out
-#     for i in range(len(aa)):
-#         if aa[i]<1e6:break
-#     ax.fill_between(range(i, len(aa)), 
-#                     [0]*(len(aa)-i),
-#                     [1]*(len(aa)-i),
-#                     color='white', 
+
+
+
+#     ##### soft white out
+#     ax.fill_between(range(i_whiteout, len(aa)),
+#                     [0]*(len(aa)-i_whiteout),
+#                     [1]*(len(aa)-i_whiteout),
+#                     color='white',
 #                     alpha=0.8
 #                    )
-    #### alternative, put a red vertical bar
-    for i in range(len(aa)):
-        if aa[i]<1e6:break
-    ax.axvline(i, color='red', alpha=0.5, linestyle='dashed')
-    
-    
+    ##### alternative: hard white out
+    ax.fill_between(range(i_whiteout, len(aa)),
+                    [0]*(len(aa)-i_whiteout),
+                    [1]*(len(aa)-i_whiteout),
+                    color='white',
+                    alpha=1, zorder=2
+                   )
+
+
     if title=='':
-        ax.set_title(f+' | type2')
+        ax.set_title(f+' | type3')
     else:
         ax.set_title(title)
     return ret, aa
+
+
+def kat_type4(f_prev, f_mid, f_now, ax,
+              write_prefix='', show=True,
+              fzero='', separator=',', ignore_lastline=True,
+              title='',
+             no_1xabove = False):
+    data = []
+    for f in [f_prev, f_mid, f_now]:
+        # accumulated ratio plot
+        with open(f) as file:
+            d = []
+            for line in file:
+                if line[0]=='#': continue
+                line = line.strip().split(separator)
+                tmp = [int(_) for _ in line if _!='']
+                tmp = [0 if _<0 else _ for _ in tmp ]  # monkey patch - need to fix the overflow
+                d.append(tmp)
+        d = np.array(d)
+        d2 = []
+        for i in range(d.shape[0]-1):
+            tmp = d[i:, :].sum(axis=0)
+            tmp = tmp/(np.sum(tmp)+1)
+            d2.append(tmp)
+        d2.append(d[-1, :]/(np.sum(d[-1, :])+1))
+        d2 = np.array(d2)
+        d2 = d2.T
+        data.append(d2)
+
+
+    LIM = 15
+    # plot new run
+    bottom = np.zeros(len(d2[0]))
+    L = len(bottom)
+    ret = ''
+    for i, _ in enumerate(d2[:LIM]):
+        if i>1:
+            alpha=0.3
+        else:
+            alpha=1
+        if i==0:
+            ret = d2[i]+bottom
+        if not no_1xabove or (no_1xabove and i<=1):
+            ax.plot(d2[i]+bottom, alpha=alpha)
+            ax.fill_between(np.arange(L), d2[i]+bottom,bottom, alpha=alpha,
+                           linewidth=0.2)
+        bottom += d2[i]
+
+    tmp = np.sum(d2[LIM:], axis=0)
+    if not no_1xabove:
+        ax.plot(tmp+bottom, alpha=alpha, color='gray')
+        ax.fill_between(np.arange(L), tmp+bottom, bottom, linewidth=0.2,
+                        alpha=alpha, color='gray')
+    # plot watermark
+    ax.fill_between(np.arange(L), data[1][0], data[0][0], hatch='/',
+                          linewidth=0.2,facecolor="#ffa154", edgecolor='black')
+    ax.fill_between(np.arange(L), data[2][0], data[1][0], hatch='\\',
+                          linewidth=0.2,facecolor="#ffc653", edgecolor='black')
+#     ax_water.plot(watermark, color='white', linewidth=1, alpha=0.7)
+
+    # absolute count right accumulated
+    a = np.sum(d, axis=1)
+    a = a[::-1]
+    aa = [a[0]]
+    for i in range(1, len(a)):
+        aa.append(aa[i-1]+a[i])
+    aa = np.array(aa[::-1])
+    for i_whiteout in range(len(aa)):
+        if aa[i_whiteout]<1e6:break
+
+    ##### absolute count right-accumulated curve
+    ax2 = ax.twinx()
+    ax2.plot(aa, color='white', linewidth=1, linestyle='dashed')
+    ax2.set_yscale('log')
+
+
+#     ##### soft white out
+#     ax.fill_between(range(i_whiteout, len(aa)),
+#                     [0]*(len(aa)-i_whiteout),
+#                     [1]*(len(aa)-i_whiteout),
+#                     color='white',
+#                     alpha=0.8
+#                    )
+    ##### alternative: hard white out
+    ax.fill_between(range(i_whiteout, len(aa)),
+                    [0]*(len(aa)-i_whiteout),
+                    [1]*(len(aa)-i_whiteout),
+                    color='white',
+                    alpha=1, zorder=2
+                   )
+
+
+    if title=='':
+        ax.set_title(f+' | type3')
+    else:
+        ax.set_title(title)
+    return ret, aa
+
 
 if __name__=='__main__':
     fig, axes = plt.subplots(1, 1)
